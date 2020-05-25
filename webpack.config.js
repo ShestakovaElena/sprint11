ade2fa235a0c6b2b45f756a355cfc78f059661d7
Shestakova Elena <el.shestakova83@gmail.com>
2020-05-25 22:50:02 +0700

    ver 1.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const WebpackMd5Hash = require('webpack-md5-hash'); 
 const webpack = require('webpack');
-new webpack.DefinePlugin({
-    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-})
+ 
+
+
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -37,16 +37,16 @@ module.exports = {
             test: /\.(eot|ttf|woff|woff2)$/,
             loader: 'file-loader?name=./vendor/[name].[ext]'
         },
-            {
-                test: /\.(png|jpg|gif|ico|svg)$/,
-                use: [
-                        'file-loader?name=./images/[name].[ext]', 
-                        {
-                            loader: 'image-webpack-loader',
-                            options: {}
-                    },   
-                ]
-         }
+        {
+            test: /\.(png|jpg|gif|ico|svg)$/,
+            use: [
+                    'file-loader?name=./images/[name].[ext]', 
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {}
+                },   
+            ]
+        }
          
         ]
     },
@@ -69,7 +69,7 @@ module.exports = {
         }),
        
         new WebpackMd5Hash(),
-         new webpack.DefinePlugin({
+        new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         })
     ]
